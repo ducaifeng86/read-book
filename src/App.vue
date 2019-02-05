d0e89064b4e901a8d8b87e440e61595c2b9b7eda
ducaifeng86 <1613195189@qq.com>
2019-02-05 20:51:27 +0800

    项目准备：添加字体图标、全局css、rem

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,28 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+		<span class="text">ABC</span>
     <router-view/>
   </div>
 </template>
-
-<style>
+<script>
+  export default {}
+	document.addEventListener('DOMContentLoaded', () => {
+	  // 获取html元素
+	  const html = document.querySelector('html')
+	  // 根据屏幕宽度动态计算fontSize
+	  let fontSize = window.innerWidth / 10
+	  // 当fontSize大于50时，设置最大值为50
+	  fontSize = fontSize > 50 ? 50 : fontSize
+	  // 设置html的fontSize属性，使rem生效，1rem = fontSize + 'px'
+	  html.style.fontSize = fontSize + 'px'
+	})
+</script>
+<style lang="scss"  scoped>
+	.text{
+		font-family: 'Days One';
+		color:red;
+	}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
