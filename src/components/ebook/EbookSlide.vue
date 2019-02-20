2ba929fc5012c859c5d66f8d7f54041259e6edf8
ducaifeng86 <1613195189@qq.com>
2019-02-21 07:34:30 +0800

    目录功能结构的布局

--- src/components/ebook/EbookSlide.vue
+++ src/components/ebook/EbookSlide.vue
@@ -4,8 +4,17 @@
 		<transition name="slide-right">
 		<div class="content" v-if="settingVisible === 3">
 			<div class="content-page-wrapper">
-				<div class="content-page"></div>
-				<div class="content-page-tab"></div>
+				<div class="content-page">
+					<component :is="currentTab === 1?content:bookmark"></component>
+				</div>
+				<div class="content-page-tab">
+					<div class="content-page-tab-item" :class="{'selected':currentTab === 1}" @click="selectTab(1)">
+						{{$t('book.navigation')}}
+					</div>
+					<div class="content-page-tab-item" :class="{'selected':currentTab === 2}" @click="selectTab(2)">
+						{{$t('book.bookmark')}}
+					</div>
+				</div>
 			</div>
 		</div>
 		</transition>
@@ -16,8 +25,22 @@
 
 <script>
 	import {ebookMixin} from '../../utils/mixin'
+	import EbookSlide from './EbookSlideContents'
 	export default{
 		mixins:[ebookMixin],
+		data(){
+			return {
+				currentTab:1,
+				content:EbookSlide,
+				bookmark:null,
+			}
+		},
+		methods:{
+			selectTab(tab){
+				this.currentTab = tab;
+			}
+		}
+		
 	}
 </script>
 
@@ -35,6 +58,29 @@
 		flex: 0 0 85%;
 		width:85%;
 		height:100%;
+		.content-page-wrapper{
+			display: flex;
+			flex-direction: column;
+			width: 100%;
+			height: 100%;
+			.content-page{
+				flex:1;
+				width:100%;
+				overflow:hidden;
+			}
+			.content-page-tab{
+				display: flex;
+				flex:0 0 px2rem(48);
+				width:100%;
+				height:px2rem(48);
+				background-color: yellow;
+				.content-page-tab-item{
+					flex:1;
+					font-size:px2rem(12);
+					@include center;
+				}
+			}
+		}
 	}
 	.content-bg{
 		flex: 0 0 15%;
---
+++ src/components/ebook/EbookSlideContents.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="ebook-slide-contents">
+		<div class="ebook-contents-search-wrapper">
+			<div class="ebook-contents-search-input-wrapper">
+				<div class="ebook-contents-search-icon">
+					<span class="icon-search"></span>
+				</div>
+				<input  class="slide-contents-search-input"
+					type="text"
+					:placeholder="$t('book.searchHint')"/>
+			</div>
+			<div class="slide-contents-search-cancel">{{$t('book.cancel')}}</div>
+		</div>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style lang='scss' scoped>
+@import '../../assets/styles/global';
+</style>
