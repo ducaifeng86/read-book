d46d3de66fccd9506f5e1f285ffae6b39beec801
ducaifeng86 <1613195189@qq.com>
2019-02-25 16:29:33 +0800

    实现鼠标支持书签的功能

--- src/components/ebook/EbookReader.vue
+++ src/components/ebook/EbookReader.vue
@@ -4,7 +4,10 @@
 		<div class="ebook-reader-mask" 
 			@click="onMaskClick"
 			@touchmove="move"
-			@touchend="moveEnd"></div>
+			@touchend="moveEnd"
+			@mousedown.left="onMouseEnter"
+			@mousemove.left="onMOuseMove"
+			@mouseup.left="onMouseEnd"></div>
 	</div>
 </template>
 
@@ -23,6 +26,42 @@
 			});
 		},
 		methods:{
+			onMouseEnter(e){
+				this.mouseState = 1;
+				this.mouseStartTime = e.timeStamp;
+				e.preventDefault();
+				e.stopPropagation();
+			},
+			onMOuseMove(e){
+				if(this.mouseState === 1){
+					this.mouseState = 2;
+				}else if(this.mouseState === 2){
+					let offsetY = 0;
+					if(this.firstOffsetY){
+						offsetY = e.clientY-this.firstOffsetY;
+						this.setOffsetY(offsetY);
+					}else{
+						this.firstOffsetY = e.clientY;
+					}
+				}
+				e.preventDefault();
+				e.stopPropagation();
+			},
+			onMouseEnd(e){
+				if(this.mouseState === 2){
+					this.setOffsetY(0);
+					this.firstOffsetY = null;
+					this.mouseState = 3;
+				}else{
+					this.mouseState = 4;
+				}
+				const time = e.timeStamp - this.mouseStartTime;
+				if(time < 100){
+					this.mouseState = 4;
+				}
+				e.preventDefault();
+				e.stopPropagation();
+			},
 			move(e){
 				let offsetY = 0;
 				if(this.firstOffsetY){
@@ -39,6 +78,9 @@
 				this.firstOffsetY = null;
 			},
 			onMaskClick(e){
+				if(this.mouseState && (this.mouseState === 2 || this.mouseState === 3)){
+					return ;
+				}
 				const offsetX = e.offsetX;
 				const width = window.innerWidth;
 				if(offsetX >0 && offsetX < width*0.3){
